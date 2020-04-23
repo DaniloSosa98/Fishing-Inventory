a44e0304e71bd7aba82b9052b2bea3a0e85c73d6
Danilososa98 <danilososa98@unitec.edu>
2020-04-22 22:22:53 -0600

    All files are now in the directory

---
+++ inventory.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "inventory.h"
+#include "trimit.h"
+
+int main(int argc, char *argv[]){
+
+    (void)argc;
+    (void)argv;
+
+}
