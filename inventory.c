9d77e850e4fad8562a72e366523beaede47f9e8b
Danilososa98 <danilososa98@unitec.edu>
2020-04-24 19:05:49 -0600

    All printing works, get, make and fulfillOrder done

--- inventory.c
+++ inventory.c
@@ -105,21 +105,24 @@ void add_assembly(inventory_t * invp, char * id, int capacity, items_needed_t * 
         a->next = invp->assembly_list;
         invp->assembly_list = a;
         invp->assembly_count++;
+        a->items = items;
     }else{
         printf("ID is invalid\n");
     }
 }
 
 void add_item(items_needed_t * items, char * id, int quantity){
-    item_t * i = lookup_item(items->item_list, id);
-    if(i == NULL){
-        printf("Item was not found\n");
-        return;
+    item_t * item = lookup_item(items->item_list, id);
+    if(item!=NULL){
+        item->quantity += quantity;
+    }else{
+        item_t * i = malloc(sizeof(item_t));
+        strcpy(i->id, id);
+        i->quantity = quantity;
+        i->next = items->item_list;
+        items->item_list = i;
+        items->item_count++;
     }
-    if(id[0] == 'P'){
-        i->quantity += quantity;
-    }
-    (void)quantity;
 }
 
 /*
@@ -139,19 +142,89 @@ item_t ** to_item_array(int count, item_t * item_list){
 int part_compare(const void *, const void *);
 int assembly_compare(const void *, const void *);
 int item_compare(const void *, const void *);
-
+*/
 void make(inventory_t * invp, char * id, int n, items_needed_t * parts){
-
+    if (n <= 0){
+        return;
+    }
+    assembly_t * a = lookup_assembly(invp->assembly_list, id);
+    if(a == NULL){
+        printf("Assembly does not exists\n");
+        return;
+    }
+    printf(">>> make %d units of assembly %s\n", n, a->id);
+    item_t * item = a->items->item_list;
+    for (int i = 0; i < a->items->item_count; i++){
+        if(lookup_part(invp->part_list, item->id) != NULL){
+            add_item(parts, item->id, n*item->quantity);
+        }else{
+            get(invp, item->id, n-item->quantity, parts);
+        }
+        item = item->next;
+    }
+    
 }
 
 void get(inventory_t * invp, char * id, int n, items_needed_t * parts){
-
+    if (n <= 0){
+        return;
+    }
+    assembly_t * a = lookup_assembly(invp->assembly_list, id);
+    if(a == NULL){
+        printf("Assembly does not exists\n");
+        return;
+    }
+    if ((a->on_hand - n) >= 0){
+        a->on_hand -= n;
+    }else{
+        make(invp, id, n - a->on_hand, parts);
+        a->on_hand = 0;
+    }
 }
 
 void print_inventory(inventory_t * invp){
+    printf("Assembly inventory:\n");
+    printf("-------------------\n");
 
+    if(invp->assembly_count == 0){
+        printf("EMPTY INVENTORY\n");
+        return;
+    }
+    printf("Assembly ID Capacity On Hand\n");
+    printf("=========== ======== =======\n");
+    assembly_t * a = invp->assembly_list;
+    for (int i = 0; i < invp->assembly_count; i++){
+        int onHand = a->on_hand;
+        int capacity = a->capacity;
+        if(onHand < (capacity/2)){
+            printf("%-11s %8d %7d*\n", a->id, capacity, onHand);
+        }else{
+            printf("%-11s %8d %7d\n", a->id, capacity, onHand);
+        }
+        a = a->next;
+    }
+    
 }
-*/
+
+void print_inventoryID(inventory_t * invp, char * id){
+    assembly_t * a = lookup_assembly(invp->assembly_list, id);
+    printf("Assembly ID:  %s\n", a->id);
+    printf("bin capacity: %d\n", a->capacity);
+    printf("on-hand:      %d\n", a->on_hand);
+    if(a->items->item_count > 0){
+        printf("Parts list:\n");
+        printf("-----------\n");
+        printf("Part ID     quantity\n");
+        printf("=========== ========\n");
+        item_t * item = a->items->item_list;
+        for (int i = 0; i < a->items->item_count; i++){
+            printf("%-11s %8d\n", item->id, item->quantity);
+            item = item->next;
+        }
+        
+        
+    }
+}
 
 void print_parts(inventory_t * invp){
     if(invp->part_count > 0){
@@ -171,11 +244,23 @@ void print_parts(inventory_t * invp){
         printf("NO PARTS\n");
     }
 }
-/*
+
 void print_items_needed(items_needed_t * items){
-
+    if(items->item_count == 0){
+        return;
+    }
+    printf("Parts needed:\n");
+    printf("-------------\n");
+    printf("Part ID     quantity\n");
+    printf("=========== ========\n");
+    item_t * item = items->item_list;
+    for (int i = 0; i < items->item_count; i++){
+        printf("%-11s %8d\n", item->id, item->quantity);
+        item = item->next;
+    }
+    
 }
-
+/*
 void free_inventory(inventory_t * invp){
 
 }
@@ -198,6 +283,7 @@ void processRequest(char * command, char ** toToken, inventory_t * invp){
 
     if (strcmp(command, "addPart") == 0){
         add_part(invp, toToken[1]);
+
     }else if (strcmp(command, "addAssembly") == 0){
         char * id = toToken[1];
         int capacity = atoi(toToken[2]);
@@ -205,12 +291,51 @@ void processRequest(char * command, char ** toToken, inventory_t * invp){
             printf("Capacity can't be negative\n");
             return;
         }
-        for (int i = 3; i < tokCount; i++){
-            
+        items_needed_t * items = malloc(sizeof(items_needed_t));
+        items->item_list = malloc(sizeof(item_t));
+        items->item_count = 0;
+        for (int i = 3; i < tokCount; i+=2){
+            char * id = toToken[i];
+            int quantity = atoi(toToken[i+1]);
+            if((lookup_assembly(invp->assembly_list, id) != NULL || lookup_part(invp->part_list, id) != NULL ) && quantity>0){
+                add_item(items, id, quantity);
+            }else{
+                printf("A parts needed condition was violated\n");
+                return;
+            }
         }
-        (void)id;
+
+        add_assembly(invp, id, capacity, items);
+
     }else if (strcmp(command, "fulfillOrder") == 0){
-        printf("fulfillOrder\n");
+        
+        items_needed_t * in = malloc(sizeof(items_needed_t));
+        in->item_list = malloc(sizeof(item_t));
+        in->item_count = 0;
+        
+        for (int i = 1; i < tokCount; i+=2){
+            char * id = toToken[i];
+            int quantity = atoi(toToken[i+1]);
+            if(lookup_assembly(invp->assembly_list, id) == NULL){
+                printf("Assembly not found\n");
+                return;
+            }
+            add_item(in, id, quantity);
+        }
+        items_needed_t * parts = malloc(sizeof(items_needed_t));
+        parts->item_list = malloc(sizeof(item_t));
+        parts->item_count = 0;
+        item_t * current = in->item_list;
+        for (int i = 0; i < in->item_count; i++){
+            char * id = current->id;
+            int quantity = current->quantity;
+            get(invp, id, quantity, parts);
+            current = current->next;
+        }
+        free(in);
+        print_items_needed(parts);
+        free(parts);
+
     }else if (strcmp(command, "stock") == 0){
         printf("stock\n");
     }else if (strcmp(command, "restock") == 0){
@@ -218,7 +343,16 @@ void processRequest(char * command, char ** toToken, inventory_t * invp){
     }else if (strcmp(command, "empty") == 0){
         empty(invp, toToken[1]);
     }else if (strcmp(command, "inventory") == 0){
-        printf("inventory\n");
+        if(tokCount>1){
+            if(lookup_assembly(invp->assembly_list, toToken[1]) != NULL){
+                print_inventoryID(invp, toToken[1]);
+            }else{
+                printf("Assembly does no exists\n");
+            }
+            
+        }else{
+            print_inventory(invp);
+        }
     }else if (strcmp(command, "parts") == 0){
         print_parts(invp);
     }else if (strcmp(command, "help") == 0){
